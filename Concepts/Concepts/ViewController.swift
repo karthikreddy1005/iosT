7d3a98add2795e5a7d553e45b784a204fc30c688
karthik-reddy <karthikb10005@gmail.com>
2024-10-05 10:21:07 +0530

    initial commit

---
+++ Concepts/Concepts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concepts
+//
+//  Created by Karthik Reddy on 05/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
